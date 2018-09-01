9b010095d9d4aa436b6b609f41a68ccf398b7d23
Byte Pair <bytepair@gmail.com>
2018-09-01 09:45:21 -0400

    Finish exercise 2

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -16,6 +16,28 @@
 
 package com.example.android.teatime;
 
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
  * corresponding OrderActivity.
@@ -26,16 +48,26 @@ package com.example.android.teatime;
  */
 
 
-// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+// COMPLETE (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class MenuActivityScreenTest {
 
-    // TODO (2) Add the rule that provides functional testing of a single activity
+    public static final String TEA_NAME = "White Tea";
 
-    // TODO (3) Finish writing this test which will click on a gridView Tea item and verify that
+    // COMPLETE (2) Add the rule that provides functional testing of a single activity
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityTestRule =
+            new ActivityTestRule<>(MenuActivity.class);
+
+    // COMPLETE (3) Finish writing this test which will click on a gridView Tea item and verify that
     // the OrderActivity opens up with the correct tea name displayed.
-
+    @Test
     public void clickGridViewItem_OpensOrderActivity() {
+        // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
+        // gridview item and clicks it.
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(2).perform(click());
 
+        // Checks that the OrderActivity opens with the correct tea name displayed
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText(TEA_NAME)));
     }
-
 }
